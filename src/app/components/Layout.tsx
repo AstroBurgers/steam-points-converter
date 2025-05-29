90d5c80fe528a6cc70e5271dff87d61bfad31267
Astro <astroburgers1@gmail.com>
2025-05-29 15:08:45 -0500

    add kofi support button thing

--- src/app/components/Layout.tsx
+++ src/app/components/Layout.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from "react";
+import Image from "next/image";
 
 interface LayoutProps {
   children: ReactNode;
@@ -11,7 +12,7 @@ const Layout = ({ children }: LayoutProps) => {
       <div className="min-h-screen bg-base-100 text-base-content">
         {/* Fixed top header */}
         <header className="fixed top-0 z-50 w-full bg-base-200 shadow-lg font-bold">
-          <div className="max-w-7xl mx-auto px-6 py-4 flex flex-col items-center space-y-1">
+          <div className="max-w-7xl mx-auto px-6 py-4 flex flex-col items-center">
             <h1 className="text-lg md:text-xl">Steam Points Converter</h1>
             <h2 className="text-sm md:text-base text-base-content/70 font-normal">
               Be ashamed with your life choices!
@@ -20,10 +21,17 @@ const Layout = ({ children }: LayoutProps) => {
         </header>
 
         {/* Push content down so it’s not under fixed header */}
-        <main className="pt-[72px] max-w-7xl mx-auto px-6">
+        <main>
           {children}
         </main>
       </div>
+      <footer className="footer bg-base-200 sm:footer-horizontal footer-center text-base-content">
+        <aside>
+          <p className="pt-4">Copyright © {new Date().getFullYear()} - All right reserved by Astro</p>
+          <a target="_blank"
+            rel="noopener noreferrer" href="https://ko-fi.com/astrotriesmodding"><Image className="pb-4 hover:scale-110 transition duration-300 hover:shadow-md" alt="Kofi Support Button" width={240} height={240} src="/support_me_on_kofi_dark.png"></Image></a>
+        </aside>
+      </footer>
     </>
   );
 };
